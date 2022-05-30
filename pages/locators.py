28998eeb7e28ad980c6266750342aa135bcbfedc
azenovs <azenovskiy@ya.ru>
2022-05-30 13:20:04 +0300

    added 4.3.13 solution

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     VIEW_BASKET = (By.CSS_SELECTOR, "span > a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators:
@@ -15,6 +16,10 @@ class BasketPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_FIELD_REGISTER = (By.CSS_SELECTOR, "[name=registration-email]")
+    PASSWORD_FIELD_REGISTER = (By.CSS_SELECTOR, "[name=registration-password1]")
+    PASSWORD_CONFIRM_FIELD_REGISTER = (By.CSS_SELECTOR, "[name=registration-password2]")
+    CONF_REGISTRATION = (By.CSS_SELECTOR, "[name=registration_submit]")
 
 
 class MainPageLocators:
--- pages/login_page.py
+++ pages/login_page.py
@@ -20,3 +20,13 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "register form is missing"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD_REGISTER)
+        email_field.send_keys(email)
+        pw1_field = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD_REGISTER)
+        pw1_field.send_keys(password)
+        pw2_field = self.browser.find_element(*LoginPageLocators.PASSWORD_CONFIRM_FIELD_REGISTER)
+        pw2_field.send_keys(password)
+        confirm_button = self.browser.find_element(*LoginPageLocators.CONF_REGISTRATION)
+        confirm_button.click()
